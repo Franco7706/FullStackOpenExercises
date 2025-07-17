51342c045bda67069aded651501372bf1ca62b6c
Franco7706 <99745409+Franco7706@users.noreply.github.com>
2025-07-17 16:57:49 +0000

    part1a exercises advanced, next todo

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,5 +1,53 @@
-import ReactDOM from 'react-dom/client'
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part.name} {props.part.exercises}</p>
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+    </div>
+  )
+}
+const Total = (props) => {
+  var total=0
+  for (const part of props.parts){
+    total+=part.exercises
+  }
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = 
+  [
+    {name:'Fundamentals of React',exercises: 10},
+    {name:'Using props to pass data',exercises: 7},
+    {name:'State of a component',exercises: 14},
+  ]
 
-import App from './App'
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+export default App
